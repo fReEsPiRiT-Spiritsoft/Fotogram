8c92b272b11739e289d9c1cf7674ecc52a8f785e
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-07-02 12:43:55 +0200

    For Loop implemented

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const bilder = [
-"./img/oldtimer1",
-"./img/oldtimer2",
-"./img/oldtimer3",
-"./img/oldtimer4",
-"./img/oldtimer5",
-"./img/oldtimer6",
-"./img/oldtimer7",
-"./img/oldtimer8"
-]
+"./img/Oldtimer1.jpg",
+"./img/Oldtimer2.jpg",
+"./img/Oldtimer3.jpg",
+"./img/Oldtimer4.jpg",
+"./img/Oldtimer5.jpg",
+"./img/Oldtimer6.jpg",
+"./img/Oldtimer7.jpg",
+"./img/Oldtimer8.jpg"
+];
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const photosSection = document.querySelector(".photos");
+
+    for (let i = 0; i < bilder.length; i++) {
+        const img = document.createElement("img");
+        img.src = [i];
+        img.alt = "Oldtimer Bild";
+        img.style.width = "200px";
+        img.style.height = "200px";
+        img.style.margin = "10px";
+        img.style.borderRadius = "10px";
+
+        photosSection.appendChild(img);
+    }
+});
